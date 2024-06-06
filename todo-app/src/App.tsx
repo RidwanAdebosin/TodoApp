a5e8565096dbe16e58b52512ea5eb6ed0fdda3cb
RidwanAdebosin <adebosinadewale@gmail.com>
2024-06-06 20:56:07 +0100

    codebase set up

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import Input from "./components/Input/Input";
+
+
+const App = () => {
+return(
+  <main className="todo-container">
+    <Header/>
+    <Input/>
+  </main>
+)
+};
+
+export default App;
---
+++ todo-app/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-h1">
+        <h1>Todo</h1>
+      </h1>
+    </header>
+  );
+};
+export default Header;
